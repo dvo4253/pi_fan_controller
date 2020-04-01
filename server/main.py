a9bbf2212383325b1a7838395e2c28bcef05e316
Nathan Dvorachek <8908406+dvo4253@users.noreply.github.com>
2020-03-31 21:51:12 -0500

    get cpu temp from file

--- server/main.py
+++ server/main.py
@@ -1,12 +1,15 @@
 from flask import Flask
-import psutil
 
 app = Flask(__name__)
 
 @app.route('/stats/cputemp')
 def index():
-    print(psutil.cpu_temperature())
-    return 'Hello world'
+    # Read CPU temperature
+    cpuTempFile = open("/sys/class/thermal/thermal_zone0/temp", "r")
+    cpuTemp = float(cpuTempFile.read()) / 1000
+    cpuTempFile.close()
+    print(cpuTemp)
+    return cpuTemp
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0') 
